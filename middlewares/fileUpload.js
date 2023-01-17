c7f40ad27698561fea2926064a6b6206c6b93be4
fuchu2003 <sayandeep18.webdev@gmail.com>
2023-01-17 21:11:34 +0530

    try to fix multer file upload problem

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -2,7 +2,7 @@ import multer, { diskStorage } from "multer";
 import path from "path";
 
 const storage = diskStorage({
-  destination: (req, file, callback) => callback(null, "uploads"),
+  destination: (req, file, callback) => callback(null, "./uploads/"),
   filename: (req, file, callback) => {
     const imageName = `${Date.now()}${path.extname(file.originalname)}`;
     callback(null, imageName);
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
-import { APP_PORT } from "./config";
+import { APP_PORT, DATABASE_URL } from "./config";
 import errorHandler from "./middlewares/errorHandler";
 import router from "./routes";
 import cors from "cors";
-require("./database/connect");
 const app = express();
 
 const port = process.env.PORT || APP_PORT;
@@ -16,4 +15,14 @@ app.use("/api", router);
 app.use(errorHandler);
 app.use("/uploads", express.static("uploads"));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  })
+  .catch((err) => {
+    console.log("Connection failed");
+    console.log(err);
+  });

3a1e49f3053df8caacdc04f205e587c37e038106
fuchu2003 <sayandeep18.webdev@gmail.com>
2023-01-23 22:03:34 +0530

    working on connection creation and chat system

--- controllers/chat/addPerson.js
+++ controllers/chat/addPerson.js
@@ -23,7 +23,7 @@ const addPerson = async (req, res, next) => {
     //check connection
     const bothUser = [req.user_id, req.body.personId];
     const chatData = await ChatModel.find({
-      users: bothUser,
+      usersId: bothUser,
     });
     if (chatData.length !== 0) {
       return next(CustomError.alreadyExist("Connection already created"));
@@ -34,17 +34,19 @@ const addPerson = async (req, res, next) => {
     );
     //create connection
     const newConnection = new ChatModel({
-      users: bothUser,
-      user_one: {
-        userId: userData._id,
-        name: userData.fullName,
-        photoUrl: userData.photoUrl,
-      },
-      user_two: {
-        userId: friend._id,
-        name: friend.fullName,
-        photoUrl: friend.photoUrl,
-      },
+      usersId: bothUser,
+      users: [
+        {
+          userId: userData._id,
+          name: userData.fullName,
+          photoUrl: userData.photoUrl,
+        },
+        {
+          userId: friend._id,
+          name: friend.fullName,
+          photoUrl: friend.photoUrl,
+        },
+      ],
     });
     await newConnection.save();
     res
--- controllers/chat/allChats.js
+++ controllers/chat/allChats.js
@@ -4,22 +4,26 @@ const allChats = async (req, res, next) => {
   try {
     //get chats
     const userChats = await ChatModel.find({
-      users: { $in: [req.user_id] },
-    });
+      usersId: { $in: [req.user_id] },
+    }).select("-usersId -__v");
     //check chat exist
     if (userChats.length === 0) {
-      res.status(204).json({ status: false, message: "No chats available" });
+      return res
+        .status(204)
+        .json({ status: false, message: "No chats available" });
     }
-    //filter out all friends id
-    const friends = [];
-    userChats.forEach((e) => {
-      const filterUsers = e.users.filter((e) => e !== req.user_id);
-      friends.push(...filterUsers);
+    //filter out current user from all chats
+    userChats.forEach((e, i) => {
+      if (e.users[0].userId === req.user_id) {
+        delete userChats[i].users.splice(0, 1);
+      } else {
+        delete userChats[i].users.splice(1, 1);
+      }
     });
-    //get all friends basic details
 
-    res.status(200).json({ status: true, message: "ok" });
+    res.status(200).json({ status: true, data: userChats });
   } catch (error) {
+    console.log(error);
     next();
   }
 };
